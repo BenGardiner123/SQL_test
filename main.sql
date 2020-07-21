491ac183fa253138327325642a87e21ae79791df
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-07-21 10:47:51 +1000

    practising some joins on my own database

--- main.sql
+++ main.sql
@@ -1,11 +1,17 @@
+SELECT b.CustomerID, c.CustomerAddress, co.CourseID, sa.StaffID
+FROM BOOKING b
+INNER JOIN COURSE_OFFERING co
+ON co.WarehouseLocation = b.WarehouseLocation
+AND co.CourseID = b.CourseID
+AND co.CourseDate = b.CourseDate
 
-INSERT INTO BOOKING (CustomerID, WarehouseLocation, CourseID, CourseDate, BookingDate, PaymentAmount, Review) VALUES
-('193840923847', 'OAKLIEGH', 'BRICK101', '2020-06-15', '2020-03-15', '500.00', NULL),
-('193834923847', 'SKYE', 'CARPENTRY102', '2020-06-15', '2020-03-12', '400.00', NULL),
-('134403563847', 'DANDENONG', 'PLUMBING103', '2020-06-15', '2020-03-23', '600.00', 'This course was amazing i especiialy like the instructor Toni');
-INSERT INTO STAFF_ALLOCATION (StaffID, CourseDate, WarehouseLocation, CourseID) VALUES
-('10000212','2020-06-15','OAKLIEGH', 'BRICK101'),
-('10000213', '2020-06-15', 'SKYE', 'CARPENTRY102'),
-('10000214','2020-06-15','DANDENONG','PLUMBING103');  
+INNER JOIN CUSTOMER c
+ON c.CustomerID = b.CustomerID 
 
-GO 
+INNER JOIN STAFF_ALLOCATION sa
+ON co.WarehouseLocation = sa.WarehouseLocation
+AND co.CourseID = sa.CourseID
+AND co.CourseDate = sa.CourseDate
+
+WHERE sa.StaffID IN (SELECT StaffID FROM STAFF 
+      WHERE StaffID = 10000214 );
